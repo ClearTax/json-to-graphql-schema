ea853f2566cf03ecc117b6f35c54fd14dbc99659
Vandan M. Shah <vandanshah77@gmail.com>
2020-07-16 17:07:44 +0530

    Update vscode version and fix issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,11 @@
 module.exports = {
   extends: ["@cleartax"],
-  overrides: [
-    {
-      files: ["**/*.test.{ts,tsx}"],
-      rules: {
-        "promise/catch-or-return": "off",
-        "promise/always-return": "off"
-      }
+  overrides: [{
+    files: ["**/*.test.{ts,tsx}"],
+    rules: {
+      "promise/catch-or-return": "off",
+      "promise/always-return": "off"
     }
-  ],
+  }],
   "eslint.workingDirectories": [{ directory: "./src", changeProcessCWD: true }]
 };
