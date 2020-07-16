a0e2531259cc7c8130eacbee3f707fa01fcd7c47
Vandan M. Shah <vandanshah77@gmail.com>
2020-07-16 19:38:50 +0530

    [Add] add image and fixes (#3)
    * [Add] add image and fixes
    
    * Update vscode version and fix issue
    
    * Code improvements

--- src/extension.ts
+++ src/extension.ts
@@ -2,33 +2,29 @@ import * as vscode from "vscode";
 import { jsonToSchema } from "@walmartlabs/json-to-simple-graphql-schema/lib";
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand(
-    "extension.convertToSchema",
-    () => {
-      // Get the current file data
-      const data =
-        vscode.window.activeTextEditor &&
-        vscode.window.activeTextEditor.document.getText();
-      // convert data into graphql schema
-      const schema = jsonToSchema({ jsonInput: data });
-      // if any error occoured show error
-      if (schema.error) {
-        vscode.window.showErrorMessage(schema.error.message);
-      }
-      if (schema.value) {
-        const content: vscode.TextDocument = schema.value;
-        vscode.window.showTextDocument(content).then((t: vscode.TextEditor) => {
-          const { selection } = t;
-          t.edit(editBuilder => {
-            editBuilder.replace(selection, content.toString());
-          });
-        });
-      }
+  const disposable = vscode.commands.registerCommand('jsontographqlschema.convertToSchema', () => {
+    // Get the current file data
+    const data =
+      vscode.window.activeTextEditor &&
+      vscode.window.activeTextEditor.document.getText();
+    // convert data into graphql schema
+    const schema = jsonToSchema({ jsonInput: data });
+    // if any error occoured show error
+    if (schema.error) {
+      vscode.window.showErrorMessage(schema.error.message);
     }
-  );
-
+    if (schema.value) {
+      const content: vscode.TextDocument = schema.value;
+      vscode.window.showTextDocument(content).then((t: vscode.TextEditor) => {
+        const { selection } = t;
+        t.edit(editBuilder => {
+          editBuilder.replace(selection, content.toString());
+        });
+      });
+    }
+  });
   context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
